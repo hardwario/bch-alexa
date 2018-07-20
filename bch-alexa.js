37935fd5d8c5b815f1f970fb5edd3bf99eadcacc
worepix <32144016+worepix@users.noreply.github.com>
2018-07-20 20:34:03 +0200

    Support mqtt msgs from client to server

--- bch-alexa.js
+++ bch-alexa.js
@@ -8,6 +8,7 @@ var messgae_aws
 var topic_aws = "";
 var message_localhost = "";
 var topic_localhost = "";
+var userid;
 
 client.on('connect', function () {
     client.subscribe("alexa/#");
@@ -16,62 +17,61 @@ client.on('connect', function () {
         else {
 
             if (process.platform == "darwin") {
-                var userid = (data.toString("utf-8")).substring(0, (data.length)-1);
+                userid = (data.toString("utf-8")).substring(0, (data.length)-1);
             }
 
             else if (process.platform == "linux") {
-                var userid = (data.toString("utf-8")).substring(0, (data.length));
+                userid = (data.toString("utf-8")).substring(0, (data.length));
             }
+
+            var device = awsIot.device({
+                keyPath: os.homedir().toString() + path.join("/bigclown-cert/") + userid + ".private.key",
+               certPath: os.homedir().toString() + path.join("/bigclown-cert/") + userid + ".cert.pem",
+                 caPath: os.homedir().toString() + path.join("/bigclown-cert/") + "root-CA.crt",
+               clientId: userid,
+                   host: "a2toxim9bns8ia.iot.eu-west-1.amazonaws.com"
+               });
             
                 client.on('message', function (topic, message){
 
+                        message_localhost = message.toString();
+                        topic_localhost = topic.toString();
+
                         if (message.toString() != messgae_aws && message.toString() != "get") {
                             device.publish((topic_localhost.replace("alexa", userid)), message.toString());
-                            message_localhost = message.toString();
-                            topic_localhost = message.toString();
                         }
-    
+
                         else if (message.toString() == "get") {
                             device.publish((((topic.toString()).replace("alexa", userid)).substring(0, (topic.toString().length-4))), message_localhost);
-                            message_localhost = message.toString();
-                            topic_localhost = topic.toString();
                         }
                         
 
                         
-                });
-
-            var device = awsIot.device({
-                 keyPath: os.homedir().toString() + path.join("/bigclown-cert/") + userid + ".private.key",
-                certPath: os.homedir().toString() + path.join("/bigclown-cert/") + userid + ".cert.pem",
-                  caPath: os.homedir().toString() + path.join("/bigclown-cert/") + "root-CA.crt",
-                clientId: userid,
-                    host: "a2toxim9bns8ia.iot.eu-west-1.amazonaws.com"
                 });
             
 
 
 
 
-            device
-                .on("connect", function() {
-                    device.subscribe('b74991e8ce894821dddb94ad802d38de/#');
-                })
+                device
+                    .on("connect", function() {
+                        device.subscribe('b74991e8ce894821dddb94ad802d38de/#');
+                    })
 
-            device
-                .on('message', function(topic, payload) {
-                    if (payload.toString() != message_localhost && payload.toString() != "get" && (topic.toString() + "/get") != topic.toString()) {
-                        client.publish(((topic.toString()).replace(userid, "alexa")), payload.toString());
+                device
+                    .on('message', function(topic, payload) {
+
                         messgae_aws = payload.toString();
                         topic_aws = topic.toString();
-                    }
 
-                    else if (payload.toString() == "get") {
-                        client.publish(((topic.toString()).replace(userid, "alexa")), payload.toString());
-                        messgae_aws = payload.toString();
-                        topic_aws = topic.toString();
-                    }
-                });
+                        if (payload.toString() != message_localhost && payload.toString() != "get" && (topic.toString() + "/get") != topic.toString()) {
+                            client.publish(((topic.toString()).replace(userid, "alexa")), payload.toString());
+                        }
+
+                        else if (payload.toString() == "get") {
+                            client.publish(((topic.toString()).replace(userid, "alexa")), payload.toString());
+                        }
+                    });
 
         }});
     });
